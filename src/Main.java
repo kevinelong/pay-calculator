2fceb817f8b22d572dd6c270dc6bd1d8e9208412
Kevin Ernest Long <kevinelong@gmail.com>
2025-04-10 14:18:01 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static double MAX_REGULAR_HOURS = 40;
+    static double OVERTIME_FACTOR = 1.5;
+
+    public static double calculatePay(double hours, double rate){
+        double regularHours = 0;
+        double overtimeHours = 0;
+        double overtimeRate = OVERTIME_FACTOR * rate;
+
+        if(hours > MAX_REGULAR_HOURS){
+            regularHours = MAX_REGULAR_HOURS;
+            overtimeHours = hours - MAX_REGULAR_HOURS;
+        }else{
+            regularHours = hours;
         }
+
+        double regularTimeTotal = regularHours * rate;
+        double overtimeTotal = overtimeHours * overtimeRate;
+        double total = regularTimeTotal + overtimeTotal;
+        System.out.printf("overtime rate: %s \n", overtimeRate);
+        System.out.printf("regular total: %s \n", regularTimeTotal);
+        System.out.printf("overtime total: %s \n", overtimeTotal);
+        return total;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("ENTER HOURS:");
+        double hours = scanner.nextDouble();
+
+        System.out.println("ENTER RATE:");
+        double rate = scanner.nextDouble();
+
+        System.out.printf("TOTAL: %.2f \n", calculatePay(hours, rate));
     }
 }
